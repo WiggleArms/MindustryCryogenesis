1e8f19f2567bcc967cca1ab9a31d46dcf950771b
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-26 07:47:33 -0400

    Java team experiment

---
+++ src/cryogenesis/DemiseTeam.java
@@ -0,0 +1,25 @@
+﻿package yourmod;
+
+import mindustry.mod.Mod;
+import mindustry.game.Team;
+import arc.graphics.Color;
+
+public class YourMod extends Mod {
+
+    public YourMod() {
+        // Constructor runs when mod is loaded
+        overrideTeam();
+    }
+
+    void overrideTeam() {
+        Team team = Team.green;
+
+        team.name = "myteam";
+        team.emoji = "⚙";
+
+        Color color = Color.valueOf("ff00ff");
+
+        team.color.set(color);
+        team.setPalette(color);
+    }
+}
