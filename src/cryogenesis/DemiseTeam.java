bb80b29f06de65fba9fc3635ad59575c481235fd
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-26 07:59:38 -0400

    Second java attempt

---
+++ src/cryogenesis/DemiseTeam.java
@@ -0,0 +1,25 @@
+﻿package cryogenesis;
+
+import mindustry.mod.Mod;
+import mindustry.game.Team;
+import arc.graphics.Color;
+
+public class DemiseTeam extends Mod {
+
+    public DemiseTeam() {
+        // Constructor runs when mod is loaded
+        overrideTeam();
+    }
+
+    void overrideTeam() {
+        Team team = Team.green;
+
+        team.name = "myteam";
+        team.emoji = "⚙";
+
+        Color color = Color.valueOf("ff00ff");
+
+        team.color.set(color);
+        team.setPalette(color);
+    }
+}
