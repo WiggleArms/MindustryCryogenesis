4259c27b7753d0d8d67c7c662fd2adb11a4a2957
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-28 09:54:54 -0400

    Core: Thread block blending change

--- src/cryogenesis/content/CryogenesisBlocks.java
+++ src/cryogenesis/content/CryogenesisBlocks.java
@@ -57,6 +57,7 @@ public class CryogenesisBlocks{
 			requirements(Category.effect, with(/*Items.silicon, 500*/));
 			alwaysUnlocked = true;
 
+			acceptsItems = true;
 			isFirstTier = true;
 			//unitType = UnitTypes.alpha;
 			health = 500;
