2ffb54f536bb3e3940ed8e2df4d8f791ec167b13
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-29 04:29:14 -0400

    Import mindustry.content.* to UnitTypes

--- src/cryogenesis/content/CryogenesisUnitTypes.java
+++ src/cryogenesis/content/CryogenesisUnitTypes.java
@@ -6,6 +6,7 @@ import arc.math.*;
 import arc.math.geom.*;
 import arc.struct.*;
 import arc.util.*;
+import mindustry.content.*;
 import mindustry.ai.*;
 import mindustry.ai.types.*;
 import mindustry.annotations.Annotations.*;
