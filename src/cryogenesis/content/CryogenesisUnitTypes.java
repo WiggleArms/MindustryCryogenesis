c427540e0614272da174b75aad1ac57244461928
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-29 06:51:37 -0400

    Reference said palette in UnitTypes

--- src/cryogenesis/content/CryogenesisUnitTypes.java
+++ src/cryogenesis/content/CryogenesisUnitTypes.java
@@ -62,7 +62,7 @@ public class CryogenesisUnitTypes{
             wreckSoundVolume = 0.8f;
             deathSoundVolume = 0.7f;
 
-            outlineColor = 10121cff;
+            outlineColor = CryogenesisPalette.ironOutline;
 
             weapons.add(new Weapon("meso-weapon"){{
                 reload = 17f;
