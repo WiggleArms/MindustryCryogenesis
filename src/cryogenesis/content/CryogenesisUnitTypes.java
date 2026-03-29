76134520458aca9436086bd95fbe39dbe89bef1a
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-29 06:39:01 -0400

    Semicolon 2: Electric Boogaloo

--- src/cryogenesis/content/CryogenesisUnitTypes.java
+++ src/cryogenesis/content/CryogenesisUnitTypes.java
@@ -60,7 +60,7 @@ public class CryogenesisUnitTypes{
             wreckSoundVolume = 0.8f;
             deathSoundVolume = 0.7f;
 
-            outlineColor = 10121cff
+            outlineColor = 10121cff;
 
             weapons.add(new Weapon("meso-weapon"){{
                 reload = 17f;
