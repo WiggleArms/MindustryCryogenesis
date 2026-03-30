49a1743edd2cab90aea2bfd50b175b1ce7472bb1
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-29 20:51:02 -0400

    Revert meso-weapon.png name change

--- src/cryogenesis/content/CryogenesisUnitTypes.java
+++ src/cryogenesis/content/CryogenesisUnitTypes.java
@@ -65,10 +65,12 @@ public class CryogenesisUnitTypes{
             faceTarget = false;
             outlineColor = CryogenesisPalette.ironOutline;
 
-            // Might be annoying
-            //loopSound = loopThoriumReactor;
-            //loopSoundVolume = 0.1;
-            //loopSoundPitch = 2;
+            /*
+            This might be annoying
+            loopSound = loopThoriumReactor;
+            loopSoundVolume = 0.1;
+            loopSoundPitch = 2;
+            */
 
             weapons.add(new Weapon("meso-weapon"){{
                 reload = 17f;
@@ -77,18 +79,6 @@ public class CryogenesisUnitTypes{
                 rotate = true;
                 shootSound = Sounds.shootMissilePlasmaShort;
                 outlineColor = CryogenesisPalette.ironOutline;
-
-                Core.app.post(() -> {
-                    Log.info("Weapon name: " + name);
-                    Log.info("Looking for region: " + name);
-                    Log.info("Found region: " + Core.atlas.find(name));
-                    Log.info("=== Atlas dump (filtered) ===");
-                    for(var region : Core.atlas.getRegions()){
-                        if(region.name.contains("meso")){
-                            Log.info("Atlas region: " + region.name);
-                    }
-                    }
-                });
 
                 bullet = new LaserBoltBulletType(2.5f, 11){{
                     keepVelocity = false;
