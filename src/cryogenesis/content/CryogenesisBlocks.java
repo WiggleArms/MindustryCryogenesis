99a5c1577afdd036921f4f7bdb9b4a1d79f95c9d
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-30 11:04:57 -0400

    Add an 's'

--- src/cryogenesis/content/CryogenesisBlocks.java
+++ src/cryogenesis/content/CryogenesisBlocks.java
@@ -75,7 +75,7 @@ public class CryogenesisBlocks{
 			health = 75;
 			speed = 30f;
 			//itemCapacity = 3;
-			bridgeReplacement = CryogenesisBlock.tunnel-pipe;
+			bridgeReplacement = CryogenesisBlocks.tunnel-pipe;
 
 			//hasLiquids = true;
 			//outputsLiquid = true;
