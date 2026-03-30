38d18f79722ebe503fec1e2b3bf4061b3b1d8523
WiggleArms <tntandtowerstav@yahoo.com>
2026-03-30 02:59:32 -0400

    Another Pipe rendering fix attempt

--- src/cryogenesis/world/blocks/pipes/Pipe.java
+++ src/cryogenesis/world/blocks/pipes/Pipe.java
@@ -1,5 +1,11 @@
 package cryogenesis.world.blocks.pipes;
 
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
 import mindustry.world.blocks.liquid.*;
 
 public class Pipe extends Conduit{
@@ -7,6 +13,17 @@ public class Pipe extends Conduit{
     public Pipe(String name){
         super(name);
 
-        // your custom tweaks here
+
+        TextureRegion result = new TextureRegion();
+        result.set(base);
+
+        float crop = base.width / 8f;
+
+        result.set(
+            (int)(base.getX() + crop),
+            (int)(base.getY() + crop),
+            (int)(base.width - crop * 2),
+            (int)(base.height - crop * 2)
+        );
     }
 }
