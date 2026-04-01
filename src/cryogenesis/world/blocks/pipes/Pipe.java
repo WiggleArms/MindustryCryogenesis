12ae852db831020a8607ac7e54f1c5888e5c85e6
WiggleArms <tntandtowerstav@yahoo.com>
2026-04-01 07:14:57 -0400

    Add missing (

--- src/cryogenesis/world/blocks/pipes/Pipe.java
+++ src/cryogenesis/world/blocks/pipes/Pipe.java
@@ -35,7 +35,7 @@ public class Pipe extends Duct{
         @Override
         public boolean acceptLiquid(Building source, Liquid liquid){
             noSleep();
-            return liquids.current() == liquid || liquids.currentAmount() < 0.2f) && (tile == null || source == this || (source.relativeTo(tile.x, tile.y) + 2) % 4 != rotation);
+            return (liquids.current() == liquid || liquids.currentAmount() < 0.2f) && (tile == null || source == this || (source.relativeTo(tile.x, tile.y) + 2) % 4 != rotation);
         }
 
         @Override
