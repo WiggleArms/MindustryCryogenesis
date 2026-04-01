d2e78112c3a78e214f9136b6e98ebe980e1b7cbf
WiggleArms <tntandtowerstav@yahoo.com>
2026-04-01 01:44:57 -0400

    Stop referencing annotations

--- src/cryogenesis/world/blocks/pipes/Pipe.java
+++ src/cryogenesis/world/blocks/pipes/Pipe.java
@@ -8,7 +8,7 @@ import arc.math.*;
 import arc.math.geom.*;
 import arc.struct.*;
 import arc.util.*;
-import mindustry.annotations.Annotations.*;
+//import mindustry.annotations.Annotations.*;
 import mindustry.world.blocks.liquid.*;
 import mindustry.world.blocks.distribution.*;
 
