07c4a272830da1246c7cf4c686233c9eaf302ca6
WiggleArms <tntandtowerstav@yahoo.com>
2026-04-05 11:28:59 -0400

    Reduce Innelis size and fix previous errors

--- src/cryogenesis/content/CryogenesisInnelisTechTree.java
+++ src/cryogenesis/content/CryogenesisInnelisTechTree.java
@@ -3,12 +3,13 @@ package cryogenesis.content;
 import arc.struct.*;
 import mindustry.game.Objectives.*;
 import mindustry.type.*;
+import mindustry.content.Items;
 
 import static mindustry.content.TechTree.*;
 
 import static cryogenesis.content.CryogenesisBlocks.*;
 import static cryogenesis.content.CryogenesisSectorPresets.*;
-import static cryogenesis.content.UnitTypes.*;
+import static cryogenesis.content.CryogenesisUnitTypes.*;
 
 public class CryogenesisInnelisTechTree{
 
@@ -17,7 +18,7 @@ public class CryogenesisInnelisTechTree{
 
 			node(pipeUnloader, () -> {
 				node(pipe, () -> {
-					node(pipeBridge);
+					node(tunnelPipe);
 				});
 			});
 
--- src/cryogenesis/content/CryogenesisPlanets.java
+++ src/cryogenesis/content/CryogenesisPlanets.java
@@ -24,7 +24,7 @@ public class CryogenesisPlanets{
 	public static void load(){
 		innelis = new Planet("innelis", Planets.sun, 1f, 2){{
 			generator = new ErekirPlanetGenerator();
-			meshLoader = () -> new HexMesh(this, 4);
+			meshLoader = () -> new HexMesh(this, 3);
 			cloudMeshLoader = () -> new MultiMesh(
 				//TODO replace with custom cloud parameters, especially change color to cryofluid-esque
                 new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
