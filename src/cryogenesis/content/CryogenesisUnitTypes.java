3ec7ea7d9c21d5f6cfd2b480b786ddd8731a02a9
WiggleArms <tntandtowerstav@yahoo.com>
2026-04-08 21:36:32 -0400

    Semicolon

--- src/cryogenesis/content/CryogenesisUnitTypes.java
+++ src/cryogenesis/content/CryogenesisUnitTypes.java
@@ -64,7 +64,7 @@ public class CryogenesisUnitTypes{
                 super.init();
 
                 commands.add(CryogenesisUnitCommand.scavengeCommand);
-            }
+            };
 
             targetBuildingsMobile = false;
             lowAltitude = true;
