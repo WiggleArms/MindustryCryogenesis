22961b6f5051be3c8df7f1dc4d303d00ee913f59
WiggleArms <tntandtowerstav@yahoo.com>
2026-04-09 12:11:40 -0400

    Bugfixes III

--- src/cryogenesis/ai/types/ScavengeAI.java
+++ src/cryogenesis/ai/types/ScavengeAI.java
@@ -17,7 +17,7 @@ import mindustry.gen.*;
 import mindustry.world.*;
 import mindustry.world.blocks.payloads.PayloadDeconstructor.*;
 import mindustry.world.meta.*;
-import mindustry.content.Blocks;
+//import mindustry.content.Blocks;
 
 import static mindustry.Vars.*;
 
@@ -71,7 +71,7 @@ public class ScavengeAI extends AIController{
 					// if current base does not exist
 					if(unloadTarget == null || !unloadTarget.isValid()){
 						// find new base
-						unloadTarget = Units.closestBuilding(unit.team, unit.x, unit.y, 160f, b -> b.block instanceof PayloadDeconstructor);
+						unloadTarget = Units.closestBuilding(unit.team, unit.x, unit.y, 160f, b -> b.block instanceof PayloadDeconstructorBuild);
 					}
 
 					// if good base
@@ -86,7 +86,7 @@ public class ScavengeAI extends AIController{
 				// if current base does not exist
 				if(unloadTarget == null || !unloadTarget.isValid()){
 					// find new base
-					unloadTarget = Units.closestBuilding(unit.team, unit.x, unit.y, 160f, b -> b.block instanceof PayloadDeconstructor);
+					unloadTarget = Units.closestBuilding(unit.team, unit.x, unit.y, 160f, b -> b.block instanceof PayloadDeconstructorBuild);
 				}
 
 				// if good base
