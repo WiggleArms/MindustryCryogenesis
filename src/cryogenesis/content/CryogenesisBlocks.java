c118bcee72ed069fecf6bae84368ba37224b46d4
WiggleArms <tntandtowerstav@yahoo.com>
2026-04-14 14:06:53 -0400

    Debugging

--- src/cryogenesis/content/CryogenesisBlocks.java
+++ src/cryogenesis/content/CryogenesisBlocks.java
@@ -87,7 +87,7 @@ public class CryogenesisBlocks{
 		}};
 		*/
 
-		oreNickel = new OreBlock(CryogenesisItems.nickel);
+		oreNickel = new OreBlock("ore-nickel", CryogenesisItems.nickel);
 
 		nickelCompactor = new GenericCrafter("nickel-compactor"){{
 			requirements(Category.crafting, with(Items.scrap, 30));
