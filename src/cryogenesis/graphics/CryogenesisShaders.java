50f0bafeeb20ab8efe4258760a57dbc1e91d5a03
WiggleArms <tntandtowerstav@yahoo.com>
2026-05-09 14:02:35 -0400

    Mimic SurfaceShader

--- src/cryogenesis/graphics/CryogenesisShaders.java
+++ src/cryogenesis/graphics/CryogenesisShaders.java
@@ -27,11 +27,40 @@ public class CryogenesisShaders{
         soda = new CryogenesisSurfaceShader("soda");
 	}
 
-	public static class CryogenesisSurfaceShader extends Shaders.SurfaceShader{
+	public static class CryogenesisSurfaceShader extends Shader{
+		Texture noiseTex;
+
 		public CryogenesisSurfaceShader(String frag){
 			super(
+				tree.get("shaders/screenspace.vert"),
 				mods.getMod("cryogenesis").root.child("shaders").child(frag + ".frag")
 			);
+			loadNoise();
 		}
+
+        public void loadNoise(){
+            Core.assets.load("sprites/" + textureName() + ".png", Texture.class).loaded = t -> {
+                t.setFilter(TextureFilter.linear);
+                t.setWrap(TextureWrap.repeat);
+            };
+        }
+
+        @Override
+        public void apply(){
+            setUniformf("u_campos", Core.camera.position.x - Core.camera.width / 2, Core.camera.position.y - Core.camera.height / 2);
+            setUniformf("u_resolution", Core.camera.width, Core.camera.height);
+            setUniformf("u_time", Time.time);
+
+            if(hasUniform("u_noise")){
+                if(noiseTex == null){
+                    noiseTex = Core.assets.get("sprites/" + textureName() + ".png", Texture.class);
+                }
+
+                noiseTex.bind(1);
+                renderer.effectBuffer.getTexture().bind(0);
+
+                setUniformi("u_noise", 1);
+            }
+        }
 	}
 }
