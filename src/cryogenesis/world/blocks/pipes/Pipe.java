71900c119c1ca71201958d037e364331afd741c5
WiggleArms <tntandtowerstav@yahoo.com>
2026-05-10 13:11:46 -0400

    Fix pipe liquid drawing

--- src/cryogenesis/world/blocks/pipes/Pipe.java
+++ src/cryogenesis/world/blocks/pipes/Pipe.java
@@ -29,6 +29,7 @@ import static mindustry.Vars.*;
 public class Pipe extends Duct{
 
     public final int timerFlow = timers++;
+    public boolean padCorners = true;
     public boolean leaks = true;
 
     public float waterBoost =  5.0f;
@@ -37,6 +38,7 @@ public class Pipe extends Duct{
     public float liquidPadding = 1f;
 
     public TextureRegion liquidRegion;
+    public TextureRegion capRegion;
 
     public Pipe(String name){
         super(name);
@@ -47,6 +49,40 @@ public class Pipe extends Duct{
     public void load(){
         super.load();
         liquidRegion = Core.atlas.find(name + "-liquid", "conduit-liquid");
+        capRegion = Core.atlas.find(name + "-cap", "conduit-cap");
+
+        rotateRegions = new TextureRegion[4][2][animationFrames];
+
+        if(renderer != null){
+            float pad = rotatePad;
+            var frames = renderer.getFluidFrames();
+
+            for(int rot = 0; rot < 4; rot++){
+                for(int fluid = 0; fluid < 2; fluid++){
+                    for(int frame = 0; frame < animationFrames; frame++){
+                        TextureRegion base = frames[fluid][frame];
+                        TextureRegion result = new TextureRegion();
+                        result.set(base);
+
+                        if(rot == 0){
+                            result.setX(result.getX() + pad);
+                            result.setHeight(result.height - pad);
+                        }else if(rot == 1){
+                            result.setWidth(result.width - pad);
+                            result.setHeight(result.height - pad);
+                        }else if(rot == 2){
+                            result.setWidth(result.width - pad);
+                            result.setY(result.getY() + pad);
+                        }else{
+                            result.setX(result.getX() + pad);
+                            result.setY(result.getY() + pad);
+                        }
+
+                        rotateRegions[rot][fluid][frame] = result;
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -78,51 +114,15 @@ public class Pipe extends Duct{
         }
     }
 
-    public static void drawTiledFrames(int size, float x, float y, float padding, Liquid liquid, float alpha){
-        drawTiledFrames(size, x, y, padding, padding, padding, padding, liquid, alpha);
+    @Override
+    public TextureRegion[] icons(){
+        return new TextureRegion[]{Core.atlas.find("-bottom", "duct-bottom"), topRegions[0]};
     }
 
-    public static void drawTiledFrames(int size, float x, float y, float padLeft, float padRight, float padTop, float padBottom, Liquid liquid, float alpha){
-        TextureRegion region = renderer.fluidFrames[liquid.gas ? 1 : 0][liquid.getAnimationFrame()];
-        TextureRegion toDraw = Tmp.tr1;
-
-        float leftBounds = size/2f * tilesize - padRight;
-        float bottomBounds = size/2f * tilesize - padTop;
-        Color color = Tmp.c1.set(liquid.color).a(1f);
-
-        for(int sx = 0; sx < size; sx++){
-            for(int sy = 0; sy < size; sy++){
-                float relx = sx - (size-1)/2f, rely = sy - (size-1)/2f;
-
-                toDraw.set(region);
-
-                //truncate region if at border
-                float rightBorder = relx*tilesize + padLeft, topBorder = rely*tilesize + padBottom;
-                float squishX = rightBorder + tilesize/2f - leftBounds, squishY = topBorder + tilesize/2f - bottomBounds;
-                float ox = 0f, oy = 0f;
-
-                if(squishX >= 8 || squishY >= 8) continue;
-
-                //cut out the parts that don't fit inside the padding
-                if(squishX > 0){
-                    toDraw.setWidth(toDraw.width - squishX * 4f);
-                    ox = -squishX/2f;
-                }
-
-                if(squishY > 0){
-                    toDraw.setY(toDraw.getY() + squishY * 4f);
-                    oy = -squishY/2f;
-                }
-
-                Drawf.liquid(toDraw, x + rightBorder + ox, y + topBorder + oy, alpha, color);
-            }
-        }
-    }
-
-    public class PipeBuild extends DuctBuild{
-
-        //might cause issues?
+    public class PipeBuild extends DuctBuild implements ChainedBuilding{
         public float smoothLiquid;
+        public int blendbits, xscl = 1, yscl = 1, blending;
+        public boolean capped, backCapped = false;
         
         @Override
         public void draw(){
@@ -132,6 +132,43 @@ public class Pipe extends Duct{
                 Draw.z(Layer.blockUnder + 0.05f);
                 drawTiledFrames(size, x, y, liquidPadding, liquids.current(), liquids.currentAmount() / liquidCapacity);
             }
+
+            Draw.z(Layer.block);
+
+            Draw.scl(xscl, yscl);
+            drawAt(x, y, blendbits, r, SliceMode.none);
+            Draw.reset();
+
+            if(capped && capRegion.found()) Draw.rect(capRegion, x, y, rotdeg());
+            if(backCapped && capRegion.found()) Draw.rect(capRegion, x, y, rotdeg() + 180);
+        }
+
+        @Override
+        protected void drawAt(float x, float y, int bits, int rotation, SliceMode slice){
+            float angle = rotation * 90f;
+            Draw.color(botColor);
+            Draw.rect(sliced(botRegions[bits], slice), x, y, angle);
+
+            int offset = yscl == -1 ? 3 : 0;
+
+            int frame = liquids.current().getAnimationFrame();
+            int gas = liquids.current().gas ? 1 : 0;
+            float ox = 0f, oy = 0f;
+            int wrapRot = (rotation + offset) % 4;
+            TextureRegion liquidr = bits == 1 && padCorners ? rotateRegions[wrapRot][gas][frame] : renderer.fluidFrames[gas][frame];
+
+            if(bits == 1 && padCorners){
+                ox = rotateOffsets[wrapRot][0];
+                oy = rotateOffsets[wrapRot][1];
+            }
+
+            //the drawing state machine sure was a great design choice with no downsides or hidden behavior!!!
+            float xscl = Draw.xscl, yscl = Draw.yscl;
+            Draw.scl(1f, 1f);
+            Drawf.liquid(sliced(liquidr, slice), x + ox, y + oy, smoothLiquid, liquids.current().color.write(Tmp.c1).a(1f));
+            Draw.scl(xscl, yscl);
+
+            Draw.rect(sliced(topRegions[bits], slice), x, y, angle);
         }
 
         @Override
@@ -167,7 +204,6 @@ public class Pipe extends Duct{
                 current = items.first();
             }
 
-            //might cause issues?
             smoothLiquid = Mathf.lerpDelta(smoothLiquid, liquids.currentAmount() / liquidCapacity, 0.05f);
 
             if(liquids.currentAmount() > 0.0001f && timer(timerFlow, 1)){
@@ -181,6 +217,10 @@ public class Pipe extends Duct{
             super.onProximityUpdate();
 
             nextc = next instanceof PipeBuild d ? d : null;
+
+            Building prev = back();
+            capped = next == null || next.team != team || !next.block.hasLiquids;
+            backCapped = blendbits == 0 && (prev == null || prev.team != team || !prev.block.hasLiquids);
         }
     }
 }
