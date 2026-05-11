d563766146c8f2b3ba1f9306245eefb91f6572e7
WiggleArms <tntandtowerstav@yahoo.com>
2026-05-10 20:41:05 -0400

    Debugging VII

--- src/cryogenesis/world/blocks/pipes/Pipe.java
+++ src/cryogenesis/world/blocks/pipes/Pipe.java
@@ -294,7 +294,8 @@ public class Pipe extends Duct{
             nextPipe = next instanceof PipeBuild d ? d : null;
             prevPipe = prev instanceof PipeBuild d ? d : null;
 
-            Building next = front(), prev = back();
+            Building next = front()
+            prev = back();
             capped = blendbits != 0 || (blendbits == 0 && next.team == team && nextPipe.blendbits != 0 && next.block instanceof Pipe);
             backCapped = blendbits != 0 || (blendbits == 0 && prev.team == team && prevPipe.blendbits != 0 && prev.block instanceof Pipe);
         }
