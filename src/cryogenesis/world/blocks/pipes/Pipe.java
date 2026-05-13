673b7e72aa57c17079505815e57b0c84f31385fe
WiggleArms <tntandtowerstav@yahoo.com>
2026-05-12 20:07:13 -0400

    Debugging III

--- src/cryogenesis/world/blocks/pipes/Pipe.java
+++ src/cryogenesis/world/blocks/pipes/Pipe.java
@@ -45,7 +45,7 @@ public class Pipe extends Duct{
     public boolean padCorners = true;
     public boolean leaks = true;
 
-    public ObjectMap boostValues = new ObjectMap<Liquid, Float>(){{
+    public ObjectMap<Liquid, Float> boostValues = new ObjectMap<>(){{
         put(Liquids.water, 3.0f);
         put(Liquids.cryofluid, 3.0f);
         put(CryogenesisLiquids.sluice, 1.2f);
