04e736f8e07fa613bd1986a0699bb0f4fdb2bdd0
WiggleArms <tntandtowerstav@yahoo.com>
2026-06-08 11:49:01 -0400

    Update Wire

--- src/cryogenesis/world/blocks/power/Wire.java
+++ src/cryogenesis/world/blocks/power/Wire.java
@@ -13,6 +13,7 @@ import mindustry.core.*;
 import mindustry.content.*;
 import mindustry.entities.*;
 import mindustry.entities.units.*;
+import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.input.*;
@@ -24,12 +25,13 @@ import mindustry.world.blocks.*;
 import mindustry.world.blocks.power.*;
 import mindustry.world.meta.*;
 
+import java.util.*;
+
 import static mindustry.Vars.*;
 
-public class Wire extends PowerDistributor implements Autotiler{
-    protected static final int stateMid = 0, stateStart = 1, stateEnd = 2
+public class Wire extends PowerBlock implements Autotiler{
 	public TextureRegion[] regions;
-	public TextureRegion connectRegion, capRegion;
+	public TextureRegion anchorRegion;
 
 	public Block bridgeReplacement;
 
@@ -40,38 +42,21 @@ public class Wire extends PowerDistributor implements Autotiler{
 
 	public Wire(String name){
 		super(name);
-		destructible = true;
-		update = false;
 		conveyorPlacement = true;
         rotate = true;
         hasShadow = false;
         solid = false;
+        destructible = true;
 	}
 
+    
 	@Override
     public void setBars(){
         super.setBars();
-        addBar("power", makePowerBalance());
-        addBar("batteries", makeBatteryBalance());
+        addBar("power", PowerNode.makePowerBalance());
+        addBar("batteries", PowerNode.makeBatteryBalance());
     }
-
-    public static Func<Building, Bar> makePowerBalance(){
-        return entity -> new Bar(() ->
-        Core.bundle.format("bar.powerbalance",
-            ((entity.power.graph.getPowerBalance() >= 0 ? "+" : "") + UI.formatAmount((long)(entity.power.graph.getPowerBalance() * 60)))),
-            () -> Pal.powerBar,
-            () -> Mathf.clamp(entity.power.graph.getLastPowerProduced() / entity.power.graph.getLastPowerNeeded())
-        );
-    }
-
-    public static Func<Building, Bar> makeBatteryBalance(){
-        return entity -> new Bar(() ->
-        Core.bundle.format("bar.powerstored",
-            (UI.formatAmount((long)entity.power.graph.getLastPowerStored())), UI.formatAmount((long)entity.power.graph.getLastCapacity())),
-            () -> Pal.powerBar,
-            () -> Mathf.clamp(entity.power.graph.getLastPowerStored() / entity.power.graph.getLastCapacity())
-        );
-    }
+    
 
 	public void load(){
 		super.load();
@@ -79,8 +64,7 @@ public class Wire extends PowerDistributor implements Autotiler{
 		for(int i = 0; i < 5; i++){
 			regions[i] = Core.atlas.find(name + "-" + i);
 		}
-        connectRegion = Core.atlas.find(name + "-connection");
-		capRegion = Core.atlas.find(name + "-cap");
+        anchorRegion = Core.atlas.find(name + "-anchor");
 	}
 
 	@Override
@@ -108,49 +92,100 @@ public class Wire extends PowerDistributor implements Autotiler{
     //TODO: Remove directionality entirely? or somehow prevent nubs 
 	@Override
 	public boolean blends(Tile tile, int rotation, int otherx, int othery, int otherrot, Block otherblock){
-		return otherblock.hasPower && !(tile.build instanceof WireBuild b && !lookingAtEither(tile, rotation, otherx, othery, otherrot, otherblock));
-	}
-
-	@Override
-	public TextureRegion[] icons(){
-		return new TextureRegion[]{regions[0]};
+        return otherblock.hasPower
+        && !(otherblock instanceof Wire && !(lookingAtEither(tile, rotation, otherx, othery, otherrot, otherblock)));
 	}
 
     protected void setupColor(float satisfaction){
         Draw.color(Tmp.c1.set(laserColor1).lerp(laserColor2, (1f - satisfaction) * 0.86f + Mathf.absin(3f, 0.1f)).a(Renderer.laserOpacity));
     }
 
+    //custom pseudoblending for anchors
+
+    public boolean anchors(Tile tile, int rotation, int otherx, int othery, int otherrot, Block otherblock){
+        return otherblock.hasPower
+        && !(otherblock instanceof Wire);
+    }
+
+    public boolean anchors(Tile tile, int rotation, int direction){
+        Building other = tile.nearbyBuild(Mathf.mod(rotation - direction, 4));
+        return other != null && other.team == tile.team() && anchors(tile, rotation, other.tileX(), other.tileY(), other.rotation, other.block);
+    }
+
+    public boolean anchors(Tile tile, int rotation, BuildPlan[] directional, int direction, boolean checkWorld){
+        int realDir = Mathf.mod(rotation - direction, 4);
+        if(directional != null && directional[realDir] != null){
+            BuildPlan req = directional[realDir];
+            if(anchors(tile, rotation, req.x, req.y, req.rotation, req.block)){
+                return true;
+            }
+        }
+        return checkWorld && anchors(tile, rotation, direction);
+    }
+
+    public int buildAnchoring(Tile tile, int rotation, BuildPlan[] directional, boolean world){
+        int anchorresult = 0;
+        for(int i = 0; i < 4; i++){
+            if(anchors(tile, rotation, directional, i, world)){
+                anchorresult |= (1 << i);
+            }
+        }
+        return anchorresult;
+    }
+
 	public class WireBuild extends Building{
-		public int blendbits, xscl, yscl, blending;
-        public Building next;
-        public WireBuild nextc;
-        public boolean capped, backCapped, connected, backConnected = false;
+		public int blendbits, xscl, yscl, anchoring;
+
+        public Building[] links = new Building[4];
+        public Tile[] dests = new Tile[4];
+        public int lastChange = -2;
+
+        
+        @Override
+        public BlockStatus status(){
+            float balance = power.graph.getPowerBalance();
+            if(balance > 0f) return BlockStatus.active;
+            if(balance < 0f && power.graph.getLastPowerStored() > 0) return BlockStatus.noOutput;
+            return BlockStatus.noInput;
+        }
+        
 
 		@Override
 		public void draw(){
             float rotation = rotdeg();
             int r = this.rotation;
-
+            
+            setupColor(power.graph.getSatisfaction());
             Draw.scl(xscl, yscl);
+            Draw.z(Layer.blockUnder);
+
             drawAt(x, y, blendbits, rotation, SliceMode.none);
+
+            Draw.color();
+            
+            //draw connection anchors to other blocks
+            Draw.z(Layer.blockUnder);
+            for(int i = 0; i < 4; i++){
+                if((anchoring & (1 << i)) != 0){
+                    int dir = r - i;
+                    Draw.rect(anchorRegion, x, y, i == 0 ? r * 90f : dir * 90f);
+                }
+            }
+
             Draw.reset();
 		}
 
 		protected void drawAt(float x, float y, int bits, float rotation, SliceMode slice){
-            Draw.z(Layer.blockUnder);
-            setupColor(power.graph.getSatisfaction());
             Draw.rect(sliced(regions[bits], slice), x, y, rotation);
-
-            if(capped && capRegion.found()) Draw.rect(capRegion, x, y, rotdeg());
-            if(backCapped && capRegion.found()) Draw.rect(capRegion, x, y, rotdeg() + 180);
-
-            Draw.color();
-            
-            //TODO: layering, cap side links
-            if(connected && connectRegion.found()) Draw.rect(connectRegion, x, y, rotdeg());
-            if(backConnected && connectRegion.found()) Draw.rect(connectRegion, x, y, rotdeg() + 180);
-
 		}
+
+        
+        @Override
+        public void pickedUp(){
+            Arrays.fill(links, null);
+            Arrays.fill(dests, null);
+        }
+        
 
 		@Override
         public void onProximityUpdate(){
@@ -160,18 +195,66 @@ public class Wire extends PowerDistributor implements Autotiler{
             blendbits = bits[0];
             xscl = bits[1];
             yscl = bits[2];
-            blending = bits[4];
-
-            Building next = front(), prev = back();
-
-            nextWire = next instanceof WireBuild d ? d : null;
-            prevWire = prev instanceof WireBuild d ? d : null;
-
-            connected = next != null && !(next instanceof WireBuild);
-            backConnected = blendbits == 0 && prev != null && !(prev instanceof WireBuild);
-
-            capped = next == null || (next != null && !next.block.squareSprite);
-            backCapped = blendbits == 0 && (prev == null || (prev != null && !prev.block.squareSprite));
+            anchoring = buildAnchoring(tile, rotation, null, true);
+            updateDirections();
         }
-	}
+
+        /*
+        @Override
+        public void updateTile(){
+            //TODO this block technically does not need to update every frame, perhaps put it in a special list.
+            if(lastChange != world.tileChanges){
+                lastChange = world.tileChanges;
+                updateDirections();
+            }
+        }
+        */
+
+        public void updateDirections(){
+            for(int i = 0; i < 4; i ++){
+                var prev = links[i];
+                var dir = Geometry.d4[i];
+                links[i] = null;
+                dests[i] = null;
+                int offset = size/2;
+
+                //find first block with power in range
+                var other = world.build(tile.x + (1 + offset) * dir.x, tile.y + (1 + offset) * dir.y);
+                if(other != null) Log.info("tile.build got from @, tilex() returns @", tile.x + (1 + offset) * dir.x, other.tileX());
+                //power nodes do NOT play nice with other links, do not touch them as that forcefully modifies their links
+                if(other != null && other.block.hasPower && !(other.block instanceof Wire && !(lookingAtEither(tile, rotation, other.tileX(), other.tileY(), other.rotation, other.block))) && !(other.block instanceof PowerNode)){
+                    links[i] = other;
+                    dests[i] = world.tile(tile.x + 1 * dir.x, tile.y + 1 * dir.y);
+                }
+
+                var next = links[i];
+
+                if(next != prev){
+                    //unlinked, disconnect and reflow
+                    if(prev != null && prev.isAdded()){
+                        prev.power.links.removeValue(pos());
+                        power.links.removeValue(prev.pos());
+
+                        PowerGraph newgraph = new PowerGraph();
+                        //reflow from this point, covering all tiles on this side
+                        newgraph.reflow(this);
+
+                        if(prev.power.graph != newgraph){
+                            //reflow power for other end
+                            PowerGraph og = new PowerGraph();
+                            og.reflow(prev);
+                        }
+                    }
+
+                    //linked to a new one, connect graphs
+                    if(next != null){
+                        power.links.addUnique(next.pos());
+                        next.power.links.addUnique(pos());
+
+                        power.graph.addGraph(next.power.graph);
+                    }
+                }
+            }
+        }
+    }
 }
